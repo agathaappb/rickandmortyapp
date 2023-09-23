55684dfa1cca1380986fc6f789365d7d83fadcef
agathaappb <agatha.alana@hotmail.com>
2023-09-22 22:03:02 -0300

    Estrutura do projeto

--- app/src/main/java/com/example/rickandmortyapp/presenter/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyapp/presenter/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.rickandmortyapp
+package com.example.rickandmortyapp.presenter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rickandmortyapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
