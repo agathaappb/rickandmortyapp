42fd87c1f58c383aac074ff175982eebeb0b5e5f
agathaappb <agatha.alana@hotmail.com>
2023-09-27 07:25:18 -0300

    Testando navegação entre fragments

--- app/src/main/java/com/example/rickandmortyapp/presenter/ui/CharacterFragment.kt
+++ app/src/main/java/com/example/rickandmortyapp/presenter/ui/CharacterFragment.kt
@@ -6,18 +6,32 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.rickandmortyapp.R
+import com.example.rickandmortyapp.databinding.FragmentCharacterBinding
+import com.example.rickandmortyapp.databinding.FragmentHomeBinding
 
 class CharacterFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentCharacterBinding? = null
+    private val binding get() = _binding!!
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_character, container, false)
+        _binding = FragmentCharacterBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
--- app/src/main/java/com/example/rickandmortyapp/presenter/ui/HomeFragment.kt
+++ app/src/main/java/com/example/rickandmortyapp/presenter/ui/HomeFragment.kt
@@ -4,20 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.rickandmortyapp.R
+import com.example.rickandmortyapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnClickme.setOnClickListener {
+            Toast.makeText(requireContext(),"Test click",Toast.LENGTH_SHORT).show()
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.container_main, CharacterFragment.newInstance())
+            transaction.disallowAddToBackStack()
+            transaction.commit()
+        }
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
